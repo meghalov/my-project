26847b54de1aa4f6a7c15cc15457646ab4a86714
Megha Lov <meghalov@gmail.com>
2024-12-22 09:41:21 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+    
+       <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  )
+}
+
+export default App
+
